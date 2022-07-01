cb366b1d7cba0084142bc798595d1292236a9929
kudagonbe <17189890+kudagonbe@users.noreply.github.com>
2022-07-02 06:41:47 +0900

    generate calendar failure test

--- gen/gencalendar.go
+++ gen/gencalendar.go
@@ -19,7 +19,7 @@ import (
 	"golang.org/x/text/transform"
 )
 
-const url = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv"
+const url = "https://www8.cao.go.jp/chosei/shukujitsu/shukujitsu.csv"
 
 func main() {
 	var minY, maxY int
